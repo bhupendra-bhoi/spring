75725f642da086cd50d69d2567b5c98bdfb74fb7
bhupen-bhoi <bhupen.nitk@gmail.com>
2019-11-19 00:28:22 +0530

    batch updated

--- spring_batch/patient-batch-loader-start/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
+++ spring_batch/patient-batch-loader-start/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/ApplicationProperties.java
@@ -9,6 +9,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+    private String inputPath = "C:/Users/bhupe/IdeaProjects/spring/spring_batch/patient-batch-loader-start/data";
+
+    public String getInputPath(){
+        return this.inputPath;
+    }
+
+    public void setInputPath(String inputPath){
+        this.inputPath = inputPath;
+    }
 
 
 }
---
+++ spring_batch/patient-batch-loader-start/src/main/java/com/pluralsight/springbatch/patientbatchloader/config/BatchJobConfiguration.java
@@ -0,0 +1,103 @@
+package com.pluralsight.springbatch.patientbatchloader.config;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.function.Function;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.PathResource;
+
+@Configuration
+public class BatchJobConfiguration {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    @Autowired
+    private ApplicationProperties applicationProperties;
+
+    @Bean
+    JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(JobRegistry jobRegistry){
+        JobRegistryBeanPostProcessor postProcessor = new JobRegistryBeanPostProcessor();
+        postProcessor.setJobRegistry(jobRegistry);
+        return postProcessor;
+    }
+
+    @Bean
+    public Job job(Step step) throws Exception{
+        return this.jobBuilderFactory
+            .get(Constants.JOB_NAME)
+            .validator(validator())
+            .start(step)
+            .build();
+    }
+
+    @Bean
+    public JobParametersValidator validator() {
+        return new JobParametersValidator() {
+            @Override
+            public void validate(JobParameters parameters) throws JobParametersInvalidException {
+                String fileName = parameters.getString(Constants.JOB_PARAM_FILE_NAME);
+                if (StringUtils.isBlank(fileName)) {
+                    throw new JobParametersInvalidException(
+                        "The patient-batch-loader.fileName parameter is required.");
+                }
+                try {
+                    Path file = Paths.get(applicationProperties.getBatch().getInputPath() +
+                        File.separator + fileName);
+                    if (Files.notExists(file) || !Files.isReadable(file)) {
+                        throw new Exception("File did not exist or was not readable");
+                    }
+                } catch (Exception e) {
+                    throw new JobParametersInvalidException(
+                        "The input path + patient-batch-loader.fileName parameter needs to " +
+                            "be a valid file location.");
+                }
+            }
+        };
+    }
+
+    @Bean
+    public Step step() throws Exception{
+        return this.stepBuilderFactory
+            .get(Constants.STEP_NAME)
+            .tasklet(new Tasklet() {
+                @Override
+                public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                    System.err.println("Hello World!");
+                    return RepeatStatus.FINISHED;
+                }
+            })
+            .build();
+    }
+
+}
